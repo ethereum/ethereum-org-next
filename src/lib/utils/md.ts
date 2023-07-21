033b1d8e750dc1f7e4710b3b17c98e6376ebdf3a
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-07-20 21:01:36 -0300

    chore: add /utils

---
+++ src/lib/utils/md.ts
@@ -0,0 +1,47 @@
+import fs from "fs"
+import { join } from "path"
+import matter from "gray-matter"
+
+import { CONTENT_DIR } from "../constants"
+
+const contentDir = join(process.cwd(), CONTENT_DIR)
+
+export const getPostSlugs = () => {
+  return fs.readdirSync(contentDir)
+}
+
+export const getContentBySlug = (slug: string, fields: string[] = []) => {
+  const realSlug = slug.replace(/\.md$/, "")
+  const fullPath = join(contentDir, `${realSlug}/index.md`)
+  const fileContents = fs.readFileSync(fullPath, "utf8")
+  const { data: frontmatter, content } = matter(fileContents)
+
+  type Items = {
+    [key: string]: string
+  }
+
+  const items: Items = {}
+
+  // Ensure only the minimal needed data is exposed
+  fields.forEach((field) => {
+    if (field === "slug") {
+      items[field] = realSlug
+    }
+    if (field === "content") {
+      items[field] = content
+    }
+
+    if (typeof frontmatter[field] !== "undefined") {
+      items[field] = frontmatter[field]
+    }
+  })
+
+  return items
+}
+
+export const getContent = (fields: string[] = []) => {
+  const slugs = getPostSlugs()
+  const content = slugs.map((slug) => getContentBySlug(slug, fields))
+
+  return content
+}
