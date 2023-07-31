55dd39e7dba25c9681d5efee072adbcdfa07009d
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-07-31 12:30:27 -0300

    chore: add temporalAllowedPages list

--- src/lib/utils/md.ts
+++ src/lib/utils/md.ts
@@ -7,19 +7,28 @@ import { CONTENT_DIR } from "../constants"
 const contentDir = join(process.cwd(), CONTENT_DIR)
 
 const getPostSlugs = (dir: string, files: string[] = []) => {
+  // Temporal list of content pages allowed to be compiled
+  // When a content page is migrated (and he components being used), should be added to this list
+  const temporalAllowedPages = ["/about", "/bridges", "/community/code-of-conduct"]
+
   // Get an array of all files and directories in the passed directory using fs.readdirSync
   const fileList = fs.readdirSync(dir)
 
   // Create the full path of the file/directory by concatenating the passed directory and file/directory name
   for (const file of fileList) {
     const name = `${dir}/${file}`
+
     // Check if the current file/directory is a directory using fs.statSync
     if (fs.statSync(name).isDirectory()) {
       // If it is a directory, recursively call the getFiles function with the directory path and the files array
       getPostSlugs(name, files)
     } else {
-      // If it is a file, push the path to the files array
-      files.push(name.replace("src/content", "").replace("/index.md", ""))
+      // If it is a file (allowed content page), push the path to the files array
+      for (const page of temporalAllowedPages) {
+        if (name.includes(page)) {
+          files.push(name.replace("src/content", "").replace("/index.md", ""))
+        }
+      }
     }
   }
 
@@ -61,6 +70,7 @@ export const getContentBySlug = (slug: string, fields: string[] = []) => {
 
 export const getContent = (fields: string[] = []) => {
   const slugs = getPostSlugs(CONTENT_DIR)
+  console.log({ slugs })
   const content = slugs.map((slug) => getContentBySlug(slug as string, fields))
 
   return content
