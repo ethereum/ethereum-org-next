d7cd4233f26a87a7f4e92ba676925d58f9dee435
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-07-31 10:11:56 -0300

    fix: nested paths

--- src/lib/utils/md.ts
+++ src/lib/utils/md.ts
@@ -6,16 +6,32 @@ import { CONTENT_DIR } from "../constants"
 
 const contentDir = join(process.cwd(), CONTENT_DIR)
 
-const getPostSlugs = () => {
-  return fs.readdirSync(contentDir)
+const getPostSlugs = (dir: string, files: string[] = []) => {
+  // Get an array of all files and directories in the passed directory using fs.readdirSync
+  const fileList = fs.readdirSync(dir)
+
+  // Create the full path of the file/directory by concatenating the passed directory and file/directory name
+  for (const file of fileList) {
+    const name = `${dir}/${file}`
+    // Check if the current file/directory is a directory using fs.statSync
+    if (fs.statSync(name).isDirectory()) {
+      // If it is a directory, recursively call the getFiles function with the directory path and the files array
+      getPostSlugs(name, files)
+    } else {
+      // If it is a file, push the path to the files array
+      files.push(name.replace("src/content", "").replace("/index.md", ""))
+    }
+  }
+
+  return files
 }
 
 // Removes {#...} from .md file so content can be parsed properly
 const removeAnchorLinks = (mdContent: string) => mdContent.replace(/{#.*?}/g, "").trim()
 
 export const getContentBySlug = (slug: string, fields: string[] = []) => {
-  const realSlug = slug.replace(/\.md$/, "")
-  const fullPath = join(contentDir, `${realSlug}/index.md`)
+  const realSlug = `${slug}/index.md`
+  const fullPath = join(contentDir, realSlug)
   const fileContents = fs.readFileSync(fullPath, "utf8")
   const { data: frontmatter, content } = matter(fileContents)
 
@@ -28,7 +44,7 @@ export const getContentBySlug = (slug: string, fields: string[] = []) => {
   // Ensure only the minimal needed data is exposed
   fields.forEach((field) => {
     if (field === "slug") {
-      items[field] = realSlug
+      items[field] = slug
     }
 
     if (field === "content") {
@@ -44,8 +60,8 @@ export const getContentBySlug = (slug: string, fields: string[] = []) => {
 }
 
 export const getContent = (fields: string[] = []) => {
-  const slugs = getPostSlugs()
-  const content = slugs.map((slug) => getContentBySlug(slug, fields))
+  const slugs = getPostSlugs(CONTENT_DIR)
+  const content = slugs.map((slug) => getContentBySlug(slug as string, fields))
 
   return content
 }
